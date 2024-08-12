189a0d7ec888343bca4cba3a1e3e2f703addb178
JacScript <jacksonsylas59@gmail.com>
2024-08-12 19:20:28 +0300

    minor changes

--- models/qnModel.js
+++ models/qnModel.js
@@ -1,30 +1,39 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
-const schema = new Schema({
-  title: {
-     type: String,
-     required: true,
+const schema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+
+    questionText: {
+      type: String,
+      required: true,
+    },
+
+    tags: [
+      {
+        type: String,
+      },
+    ],
+
+    //(references the Answer model)
+    answers: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Answer",
+        required: true,
+      },
+    ],
   },
-
-  questionText: {
-    type: String,
-    required: true,
-  },
-
-  //(references the Answer model)
-  answers:[{
-    type: Schema.Types.ObjectId,
-    ref: "Answer",
-    required: true,
-  }],
-}, {timestamps: true});
-
+  { timestamps: true }
+);
 
 // schema timestamps indexing
-schema.index({ createdAt: -1 }, { background: true })
-schema.index({ updatedAt: -1 }, { background: true })
+schema.index({ createdAt: -1 }, { background: true });
+schema.index({ updatedAt: -1 }, { background: true });
 
-const Question = model('Question', schema);
-
+const Question = model("Question", schema);
 
 module.exports = Question;
--- routes.js
+++ routes.js
@@ -8,14 +8,14 @@ const { mongoose } = require("mongoose");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 var nodemailer = require("nodemailer");
-const genarateToken = require('./utils/generateToken.js')
+const genarateToken = require("./utils/generateToken.js");
 
 //import files
 
 const Question = require("./models/qnModel.js");
 const Answer = require("./models/ansModel.js");
 const User = require("./models/userModel.js");
-const Tag = require("./models/tagModel.js")
+const Tag = require("./models/tagModel.js");
 const generateToken = require("./utils/generateToken.js");
 
 // Route to get all questions (with populated answers)
@@ -36,9 +36,9 @@ router.get("/questions", async (request, response) => {
 //Route for creating questions
 router.post("/questions", async (request, response, next) => {
   try {
-    const token = request.cookies;
-    const { title, questionText } = request.body;
-    const newQuestion = new Question({ title, questionText });
+    // const token = request.cookies;
+    const { title, questionText, tags } = request.body;
+    const newQuestion = new Question({ title, questionText, tags });
     await newQuestion.save();
 
     // Sort logic: Replace this with your actual sorting criteria
@@ -46,14 +46,13 @@ router.post("/questions", async (request, response, next) => {
     questions.push(newQuestion); // Add new question to the array
     questions.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt)); // Sort by creation time (descending)
 
-    console.log(token)
+    // console.log(token)
     // Save all questions (including the new one)
     await Promise.all(questions.map((question) => question.save()));
     response.status(201).json({
       message: "Question created and sorted successfully",
       question: newQuestion,
-    }
-  );
+    });
   } catch (err) {
     response
       .status(500)
@@ -293,8 +292,6 @@ router.post("/auth/signup", async (request, response) => {
       return response.json({ message: "User Already existed" });
     }
 
-  
-
     const hashpassword = await bcrypt.hash(password, 10);
     const newUser = new User({
       username,
@@ -304,11 +301,16 @@ router.post("/auth/signup", async (request, response) => {
 
     await newUser.save();
 
-    const token = jwt.sign({id: User._id }, process.env.KEY, {
+    const token = jwt.sign({ id: User._id }, process.env.KEY, {
       expiresIn: "30m",
     });
     // response.cookie("token", token, { httpOnly: true, maxAge: 360000 });
-    return response.json({ status: true, message: "Record registed" , user: newUser , token:token});
+    return response.json({
+      status: true,
+      message: "Record registed",
+      user: newUser,
+      token: token,
+    });
   } catch (err) {
     return response
       .status(500)
@@ -317,7 +319,6 @@ router.post("/auth/signup", async (request, response) => {
 });
 
 router.post("/auth/login", async (request, response) => {
-  
   try {
     const { email, password } = request.body;
     const user = await User.findOne({ email });
@@ -332,12 +333,17 @@ router.post("/auth/login", async (request, response) => {
       return response.status(401).json({ message: "password incorrect" });
     }
 
-    const token = jwt.sign({id: user._id }, process.env.KEY, {
+    const token = jwt.sign({ id: user._id }, process.env.KEY, {
       expiresIn: "30m",
     });
     response.cookie("token", token, { httpOnly: true, maxAge: 360000 });
 
-    return response.json({ status: true, message: "Login Succesffuly", user, token: token});
+    return response.json({
+      status: true,
+      message: "Login Succesffuly",
+      user,
+      token: token,
+    });
   } catch (err) {
     return response
       .status("500")
@@ -345,64 +351,62 @@ router.post("/auth/login", async (request, response) => {
   }
 });
 
-router.post("/auth/forgotPassword", async (request, response) => {
-  const { email } = request.body;
+// router.post("/auth/forgotPassword", async (request, response) => {
+//   const { email } = request.body;
 
-  try {
-    const user = await User.findOne({ email });
+//   try {
+//     const user = await User.findOne({ email });
 
-    if (!user) {
-      return response.status(404).json({ mesage: "User not registered" });
-    }
+//     if (!user) {
+//       return response.status(404).json({ mesage: "User not registered" });
+//     }
 
-    const token = jwt.sign({id: user._id}, process.env.KEY, {expiresIn: '5m'})
+//     const token = jwt.sign({ id: user._id }, process.env.KEY, {
+//       expiresIn: "5m",
+//     });
 
-    var transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL,
-        pass: process.env.PASSWORD,
-      },
-    });
+//     var transporter = nodemailer.createTransport({
+//       service: "gmail",
+//       auth: {
+//         user: process.env.EMAIL,
+//         pass: process.env.PASSWORD,
+//       },
+//     });
 
-    var mailOptions = {
-      from: process.env.EMAIL,
-      to: email,
-      subject: "Reset Password",
-      text: `http://localhost:5173/resetPassword/${token}`,
-    };
+//     var mailOptions = {
+//       from: process.env.EMAIL,
+//       to: email,
+//       subject: "Reset Password",
+//       text: `http://localhost:5173/resetPassword/${token}`,
+//     };
 
-    transporter.sendMail(mailOptions, function (error) {
-      if (error) {
-        return response.json({ message: "error sending email" });
-      } else {
-        return response.json({ status: true, messsage: "Email sent" });
-      }
-    });
-  } catch (error) {
-    console.log(error);
-  }
-});
+//     transporter.sendMail(mailOptions, function (error) {
+//       if (error) {
+//         return response.json({ message: "error sending email" });
+//       } else {
+//         return response.json({ status: true, messsage: "Email sent" });
+//       }
+//     });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
 
-router.post("/auth/resetPassword/:token", async(request, response) => {
-    const {token } = request.params;
-    const  {password} = request.body;
+// router.post("/auth/resetPassword/:token", async (request, response) => {
+//   const { token } = request.params;
+//   const { password } = request.body;
 
-    try {
-
-      const decoded = await jwt.verify(token , process.env.KEY); 
-      const id = decoded.id;
-      const hashPassword = await bcrypt.hash(password, 10);
-      await User.findByIdAndUpdate({_id:id}, {password: hashPassword})
-      return response.json({status: true, message: "Password Updated"})
-      
-    } catch (err) {
-      console.error(err);
-      return response.json({message: "Invalid token"})
-    }
-
-});
-
+//   try {
+//     const decoded = await jwt.verify(token, process.env.KEY);
+//     const id = decoded.id;
+//     const hashPassword = await bcrypt.hash(password, 10);
+//     await User.findByIdAndUpdate({ _id: id }, { password: hashPassword });
+//     return response.json({ status: true, message: "Password Updated" });
+//   } catch (err) {
+//     console.error(err);
+//     return response.json({ message: "Invalid token" });
+//   }
+// });
 
 // Function to verify the token (replace with your secret key)
 // const verifyToken = (req, res, next) => {
@@ -419,65 +423,42 @@ router.post("/auth/resetPassword/:token", async(request, response) => {
 //   });
 // };
 
-
-
-router.get("/profile/", async(request, response) => {
-  try {
-    // Get token from cookie
-    // const token = request.params.token;
-    const token = request.cookies.token;
-
-    // Check if token exists
-    if (!token) {
-      return response.status(401).json({ message: 'Unauthorized: Missing access token' });
-    }
-
-    // Verify the token using JWT secret (replace with your secret)
-    const decoded = jwt.verify(token, process.env.KEY);
-
-    // Get the user ID from the decoded token
-    const id = require.decoded.id;
-
-    // Find the user by ID in MongoDB
-    const user = await User.findById(id);
-
-    // Check if user exists 
-    if (!user) {
-      return response.status(404).json({ message: 'User not found' });
-    }
-
-    // Return the username
-    return response.status(200).json({user: user.username});
-  } catch (err) {
-    console.error(err);
-   return response.status(500).json({ message: 'Internal server error' });
-  }
-});
-
-
-
-// router.post("/tags", async (request, response) => {
+// router.get("/profile/", async (request, response) => {
 //   try {
-//     const { name } = request.body;
+//     // Get token from cookie
+//     // const token = request.params.token;
+//     const token = request.cookies.token;
 
-//     const tag = new Tag({ name });
-//     const savedTag = await tag.save();
-//     response.status(201).json({ message: savedTag });
-//   } catch (error) {
-//     console.log(error);
-//     response.status(500).json({ message: error.message });
+//     // Check if token exists
+//     if (!token) {
+//       return response
+//         .status(401)
+//         .json({ message: "Unauthorized: Missing access token" });
+//     }
+
+//     // Verify the token using JWT secret (replace with your secret)
+//     const decoded = jwt.verify(token, process.env.KEY);
+
+//     // Get the user ID from the decoded token
+//     const id = require.decoded.id;
+
+//     // Find the user by ID in MongoDB
+//     const user = await User.findById(id);
+
+//     // Check if user exists
+//     if (!user) {
+//       return response.status(404).json({ message: "User not found" });
+//     }
+
+//     // Return the username
+//     return response.status(200).json({ user: user.username });
+//   } catch (err) {
+//     console.error(err);
+//     return response.status(500).json({ message: "Internal server error" });
 //   }
 // });
 
-router.get('/tags', async (request, response) => {
-  try {
-    const tags = await Tag.find();
-    response.status(201).json(tags);
-  } catch (error) {
-    console.error(error);
-    response.status(500).json({ error: 'Error fetching tags' });
-  }
-});
+
 
 
 module.exports = router;
